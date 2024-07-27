ba7a57709ba3118027b9cb7f39eceb59810c7618
Kiều Uy Vũ <141432067+dillysluvu@users.noreply.github.com>
2024-07-27 21:33:42 +0700

    Delete V1/nvim/lua/plugins/overseer.lua

--- V1/nvim/lua/plugins/overseer.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "stevearc/overseer.nvim",
-  config = function()
-    require("overseer").setup({
-      templates = { "builtin", "user.cpp_build", "user.run_script" },
-    })
-  end,
-}
