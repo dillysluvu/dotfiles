4110909c809eda3a4648c7a7e6cebaa36cd723f6
Kiều Uy Vũ <141432067+dillysluvu@users.noreply.github.com>
2024-08-02 20:43:12 +0700

    Update sp.sh

--- sp.sh
+++ sp.sh
@@ -12,14 +12,16 @@ update_system() {
 install_packages() {
     local packages=(
         git npm rust curl make bison gcc glibc zsh fzf ripgrep fd bat wezterm tmux
-        libevent ncurses base-devel pkgconf neovim unrar 
+        libevent ncurses base-devel pkgconf neovim unrar
     )
     sudo pacman -S --noconfirm "${packages[@]}"
 }
 
 # Function to install Zsh and Oh My Zsh
 install_zsh() {
-    sudo pacman -S --noconfirm zsh
+    if ! command -v zsh &> /dev/null; then
+        sudo pacman -S --noconfirm zsh
+    fi
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -84,56 +86,75 @@ configure_user() {
     esac
 }
 
+# Function to handle Fedora Linux base and ASUS laptop specific setup
+setup_asus_laptop() {
+    read -p "Do you use Fedora Linux base and ASUS laptop? (yes/no): " response
+    if [[ "$response" == "yes" ]]; then
+        local packages=(
+            cmake clang-devel libinput-devel libseat-devel libgbm-devel libxkbcommon-devel
+            systemd-devel libdrm-devel expat-devel pcre2-devel libzstd-devel gtk3-devel base-devel git
+        )
+        sudo pacman -S --noconfirm "${packages[@]}"
+        sudo pacman -S power-profiles-daemon
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+        yay -S asusctl supergfxctl rog-control-center
+        cd ..
+        rm -rf yay
+
+        sudo systemctl enable --now power-profiles-daemon.service
+        sudo systemctl enable --now supergfxd.service
+        sudo systemctl enable --now asusd.service
+    fi
+}
+
 # Function to install Flatpak and add Flathub repository
 setup_flatpak() {
     read -p "Do you want to install some Flatpak apps? (yes/no): " install_flatpak
     if [[ "$install_flatpak" == "yes" ]]; then
         sudo pacman -S --noconfirm flatpak
         sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-        
-        install_flatpak_apps() {
-            local apps=(
-                "app.drey.Warp"
-                "com.github.KRTirtho.Spotube"
-                "com.github.flxzt.rnote"
-                "com.github.neithern.g4music"
-                "com.github.tchx84.Flatseal"
-                "com.github.wwmm.easyeffects"
-                "com.github.jakeman.ExtensionManager"
-                "dev.vencord.Vesktop"
-                "garden.jamie.Morphosis"
-                "io.bassi.Amberol"
-                "io.github.getnf.embellish"
-                "com.github.wiiznokes.fan-control"
-                "io.gitlab.adhami3310.Converter"
-                "io.github.missioncenter.MissionCenter"
-                "it.mijorus.gearlever"
-                "org.freac.freac"
-                "org.gnome.Extensions"
-                "org.gnome.gitlab.somas.Apostrophe"
-            )
-        
-            read -p "Would you like to install all Flatpak apps? (yes/no): " install_all
-        
-            if [[ "$install_all" == "yes" ]]; then
-                for app in "${apps[@]}"; do
+
+        local apps=(
+            "app.drey.Warp"
+            "com.github.KRTirtho.Spotube"
+            "com.github.flxzt.rnote"
+            "com.github.neithern.g4music"
+            "com.github.tchx84.Flatseal"
+            "com.github.wwmm.easyeffects"
+            "com.github.jakeman.ExtensionManager"
+            "dev.vencord.Vesktop"
+            "garden.jamie.Morphosis"
+            "io.bassi.Amberol"
+            "io.github.getnf.embellish"
+            "com.github.wiiznokes.fan-control"
+            "io.gitlab.adhami3310.Converter"
+            "io.github.missioncenter.MissionCenter"
+            "it.mijorus.gearlever"
+            "org.freac.freac"
+            "org.gnome.Extensions"
+            "org.gnome.gitlab.somas.Apostrophe"
+        )
+
+        read -p "Would you like to install all Flatpak apps? (yes/no): " install_all
+
+        if [[ "$install_all" == "yes" ]]; then
+            for app in "${apps[@]}"; do
+                flatpak install -y flathub "$app"
+            done
+            echo "All apps have been installed."
+        else
+            for app in "${apps[@]}"; do
+                read -p "Do you want to install $app? (yes/no): " response
+                if [[ "$response" == "yes" ]]; then
                     flatpak install -y flathub "$app"
-                done
-                echo "All apps have been installed."
-            else
-                for app in "${apps[@]}"; do
-                    read -p "Do you want to install $app? (yes/no): " response
-                    if [[ "$response" == "yes" ]]; then
-                        flatpak install -y flathub "$app"
-                    else
-                        echo "Skipping $app"
-                    fi
-                done
-                echo "Selected apps have been installed."
-            fi
-        }
-        
-        install_flatpak_apps
+                else
+                    echo "Skipping $app"
+                fi
+            done
+            echo "Selected apps have been installed."
+        fi
     else
         echo "Skipping Flatpak app installations."
     fi
@@ -149,7 +170,8 @@ install_tmux
 install_starship
 install_version_managers
 configure_user
+setup_asus_laptop
 setup_flatpak
 
-# Source the new .zshrc
-source ~/.zshrc
+# Source the new .zshrc if it exists
+[ -f ~/.zshrc ] && source ~/.zshrc
