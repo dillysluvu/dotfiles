b50ae3180146a7bbb379bcdfb91746bded0a0f14
Kiều Uy Vũ <141432067+dillysluvu@users.noreply.github.com>
2024-09-10 22:36:46 +0700

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,42 +1,55 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Catppuccin Mocha",
-	--color_scheme = "Twilight (dark) (terminal.sexy)",
+  color_scheme = "Solarized Osaka",
+  colors = {
+    foreground = "#839496", -- s:base0
+    background = "#002b36", -- s:base03
+    cursor_bg = "#839496",  -- s:base0
+    cursor_border = "#839496", -- s:base0
+    cursor_fg = "#002b36", -- s:base03
+    selection_bg = "#073642", -- s:base02
+    selection_fg = "#839496", -- s:base0
 
-	enable_tab_bar = false,
-	font_size = 16,
-	font = wezterm.font("JetBrains Mono"),
-	macos_window_background_blur = 40,
-	-- macos_window_background_blur = 30,
+    ansi = {
+      "#002b36", "#dc322f", "#859900", "#b58900",
+      "#268bd2", "#d33682", "#2aa198", "#eee8d5"
+    },
+    brights = {
+      "#073642", "#cb4b16", "#586e75", "#657b83",
+      "#6c71c4", "#d33682", "#2aa198", "#fdf6e3"
+    },
 
-	-- window_background_image = "/home/mcv/Pictures/meo.png",
-	-- window_background_image_hsb = {
-	--   brightness = 0.01,
-	--   hue = 1.0,
-	--   saturation = 0.5,
-	-- },
+    tab_bar = {
+      background = "#002b36",
+      active_tab = { bg_color = "#657b83", fg_color = "#fdf6e3" },
+      inactive_tab = { bg_color = "#073642", fg_color = "#839496" },
+      inactive_tab_hover = { bg_color = "#586e75", fg_color = "#fdf6e3", italic = true },
+      new_tab = { bg_color = "#073642", fg_color = "#93a1a1" },
+      new_tab_hover = { bg_color = "#586e75", fg_color = "#fdf6e3", italic = true },
+    },
+  },
 
-	window_background_opacity = 0.92,
-	-- window_background_opacity = 1,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	window_decorations = "NONE",
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 16,
+  window_background_opacity = 0.92,
+  window_decorations = "NONE",
+  enable_tab_bar = false,
 
-	keys = {
-		{ key = "f", mods = "CTRL", action = wezterm.action.ToggleFullScreen },
-		{ key = "q", mods = "CTRL", action = wezterm.action.QuitApplication },
-		{ key = "h", mods = "SUPER", action = wezterm.action.Hide },
-		{ key = "v", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "h", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	},
+  keys = {
+    { key = "f", mods = "CTRL", action = wezterm.action.ToggleFullScreen },
+    { key = "q", mods = "CTRL", action = wezterm.action.QuitApplication },
+    { key = "h", mods = "SUPER", action = wezterm.action.Hide },
+    { key = "v", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "h", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  },
 
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
 }
+
